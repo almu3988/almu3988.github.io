54b24d4dafb666e390ece479c87969f648a05ad2
almu3988 <almu3988@colorado.edu>
2023-11-12 22:42:14 -0700

    zz
    xx

---
+++ js/wa11.js
@@ -0,0 +1,27 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const images = ['pic1.jgp', 'pic2.jpg', 'pic3.jgp','pic4.jgp','pic5.jgp'];
+
+/* Declaring the alternative text for each image file */
+const alts = {
+    'pic1.jgp': 'ccc',
+    'pic2.jgp': 'dwd',
+    'pic3.jgp': 'ffefe',
+    'pic4.jgp': 'dfg',
+    'pic5.jgp': 'erfe'
+};
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', pic1.jpg, pic2.jpg,);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+dcsjdnsdjsdndsjjnj
+
+/* Wiring up the Darken/Lighten button */
