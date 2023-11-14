ac0755b7e0192c3c801b6b1e9f9c14fd0143568e
almu3988 <almu3988@colorado.edu>
2023-11-14 09:11:20 -0700

    bbhb

--- js/wa11.js
+++ js/wa11.js
@@ -1,27 +1,43 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
-const images = ['pic1.jgp', 'pic2.jpg', 'pic3.jgp','pic4.jgp','pic5.jgp'];
+const alts = ['A mountain surrounded by fog.','a green and pink spiral','a very cute bunny','an ocean wave.','a field of California Poppies.'];
 
-/* Declaring the alternative text for each image file */
-const alts = {
-    'pic1.jgp': 'ccc',
-    'pic2.jgp': 'dwd',
-    'pic3.jgp': 'ffefe',
-    'pic4.jgp': 'dfg',
-    'pic5.jgp': 'erfe'
-};
+/* img loop access */
+  function thumbnailLoop() {
+  var i;
+  for (i = 0; i < 5; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', "/img/pic" + (i+1) + ".jpg");
+  newImage.setAttribute('alt',alts[i]);
+  thumbBar.appendChild(newImage);
+}
+}
 
-/* Looping through images */
+thumbnailLoop();
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', pic1.jpg, pic2.jpg,);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-dcsjdnsdjsdndsjjnj
+/* click thumbnail --> main img */
+thumbBar.addEventListener('click', (e) => {
+  if(e.target && e.target.nodeName == "IMG") {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+}
+}); 
 
-/* Wiring up the Darken/Lighten button */
+/* darken/lighten button */
+btn.addEventListener('click', (e) => {
+  if (e.target.className == 'dark') {
+    btn.setAttribute('class', 'light');
+    btn.textContent = 'lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+}
+
+ else {
+    btn.setAttribute('class', 'dark');
+    btn.textContent = 'darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+}
+});
