7cf15d48b165eed38631ec07d8359fd640d9f59f
almu3988 <almu3988@colorado.edu>
2023-11-14 09:24:05 -0700

    mm

--- js/wa11.js
+++ js/wa11.js
@@ -4,37 +4,41 @@ var thumbBar = document.querySelector('.thumb-bar');
 btn = document.querySelector('button');
 var overlay = document.querySelector('.overlay');
 
-const alts = ['Ocean waves.','Very blue ocean','Ocean with fish','Ocean with sunny view','A perfect wave'];
 
-/* img loop access */
-  function thumbnailLoop() {
-  var i;
-  for (i = 0; i < 5; i++) {
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', "../img/pic" + (i+1) + ".jpg");
-  newImage.setAttribute('alt',alts[i]);
-  thumbBar.appendChild(newImage);
-}
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const alts = {
+  'pic1.jpg' : 'Ocean waves.',
+  'pic2.jpg' : 'Very blue ocean',
+  'pic3.jpg' : 'Ocean with fish',
+  'pic4.jpg' : 'Ocean with sunny view',
+  'pic5.jpg' : 'A perfect wave'
 }
 
-thumbnailLoop();
-/* click thumbnail --> main img */
-thumbBar.addEventListener('click', (e) => {
-  if(e.target && e.target.nodeName == "IMG") {
-    displayedImage.src = e.target.src;
-    displayedImage.alt = e.target.alt;
-}
-}); 
-/* darken/lighten button */
-btn.addEventListener('click', (e) => {
-  if (e.target.className == 'dark') {
-    btn.setAttribute('class', 'light');
-    btn.textContent = 'lighten';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-}
- else {
-    btn.setAttribute('class', 'dark');
-    btn.textContent = 'darken';
-    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-}
-});
+
+/* Looping through images */
+
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alts[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
+  
+  /* Wiring up the Darken/Lighten button */
+  
+  btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
